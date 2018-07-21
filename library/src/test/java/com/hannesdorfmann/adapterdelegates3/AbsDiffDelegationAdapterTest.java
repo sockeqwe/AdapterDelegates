dddd7408cf2ce5d7e0ac5480b2e2da3efaaec9e9
Sergey Opivalov <=>
2018-07-21 17:57:35 +0300

    Tests fix with robolectric

--- library/src/test/java/com/hannesdorfmann/adapterdelegates3/AbsDiffDelegationAdapterTest.java
+++
@@ -1,8 +0,0 @@
-package com.hannesdorfmann.adapterdelegates3;
-
-/**
- * Created by six_hundreds on 28.06.18.
- */
-
-public class AbsDiffDelegationAdapterTest {
-}
--- library/src/test/java/com/hannesdorfmann/adapterdelegates3/DiffDelegationAdapterTest.java
+++ library/src/test/java/com/hannesdorfmann/adapterdelegates3/DiffDelegationAdapterTest.java
@@ -4,10 +4,15 @@ import android.support.v7.recyclerview.extensions.AsyncListDiffer;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 /**
  * Created by six_hundreds on 28.06.18.
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest=Config.NONE)
 public class DiffDelegationAdapterTest {
 
     @Test
