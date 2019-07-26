280c628642f090a3d1029667dd2071d7fe3e4255
Till Hellmund <thellmund@gmail.com>
2019-07-26 08:31:11 +0200

    Fix broken tests in LayoutContainerListAdapterDelegateDslTest by mocking Context

--- kotlin-dsl-layoutcontainer/src/test/java/com/hannesdorfmann/adapterdelegates4/LayoutContainerListAdapterDelegateDslTest.kt
+++ kotlin-dsl-layoutcontainer/src/test/java/com/hannesdorfmann/adapterdelegates4/LayoutContainerListAdapterDelegateDslTest.kt
@@ -1,15 +1,14 @@
 package com.hannesdorfmann.adapterdelegates4
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-import androidx.recyclerview.widget.RecyclerView
 import com.hannesdorfmann.adapterdelegates4.dsl.AdapterDelegateLayoutContainerViewHolder
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
 import org.junit.Assert
 import org.junit.Test
 import org.mockito.Mockito
-import java.lang.IllegalStateException
+import org.mockito.Mockito.`when` as whenever
 
 class LayoutContainerListAdapterDelegateDslTest {
 
@@ -18,6 +17,8 @@ class LayoutContainerListAdapterDelegateDslTest {
     private fun fakeLayoutInflater(layoutToInflate: Int): Pair<(ViewGroup, Int) -> View, ViewGroup> {
         val viewGroup = Mockito.mock(ViewGroup::class.java)
         val view = Mockito.mock(View::class.java)
+        val context = Mockito.mock(Context::class.java)
+        whenever(view.context).thenReturn(context)
 
         val inflater = { parent: ViewGroup, layoutRes: Int ->
             Assert.assertSame(viewGroup, parent)
